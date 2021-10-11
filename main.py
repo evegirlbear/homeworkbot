8d543c2d8cb16a72cbfb14ebb53a90749d7bcb21
64070096 <64070096@it.kmitl.ac.th>
2021-10-11 21:03:28 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# 897087395177054278
+# ODk3MDg3Mzk1MTc3MDU0Mjc4.YWQjvg.1_Jef0zy6GgGsjC7J1lEGbwvtTI
+# 534723950656
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f"Link start!!! {client.user}")
+
+
+client.run('ODk3MDg3Mzk1MTc3MDU0Mjc4.YWQjvg.1_Jef0zy6GgGsjC7J1lEGbwvtTI')
