739dbc1b7ab3feb66da953220f0f69a1bfa140a7
64070096 <64070096@it.kmitl.ac.th>
2021-10-11 20:22:48 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,12 +1,23 @@
-print("Hello")
+# 897087395177054278
+# ODk3MDg3Mzk1MTc3MDU0Mjc4.YWQjvg.1_Jef0zy6GgGsjC7J1lEGbwvtTI
+# 534723950656
+import discord
 
-print("Psit")
-print("psitgetA")
+client = discord.Client()
 
-print("mar young")
-print("2543eiei")
-print("evelovechoco")
-print("eieiei")
 
-print(":)")
+@client.event
+async def on_ready():
+    print(f"Link start!!! {client.user}")
 
+
+@client.event
+async def on_message(message):
+    if message.content == "!homework":
+        print(message.channel)
+        await message.channel.send('ยังไม่ได้ทำ')
+    elif message.content == "!logout":
+        await client.logout()
+
+
+client.run('ODk3MDg3Mzk1MTc3MDU0Mjc4.YWQjvg.1_Jef0zy6GgGsjC7J1lEGbwvtTI')
