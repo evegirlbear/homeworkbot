13ba88686147fae64f67c68449634dbfd0ddd7c9
64070096 <64070096@it.kmitl.ac.th>
2021-10-18 21:19:23 +0700

    64070096

--- commands/help.py
+++ commands/help.py
@@ -19,7 +19,9 @@ class Help(commands.Cog, name="Help"):
         embed.add_field(name="ตัวอย่างการพิมพ์คำสั่ง :",value="```pip!author, pip!homework```", inline=False)
         embed.add_field(name="คำสั่งทั่วไป  :",value="```pip!add : ทำการเพิ่มการบ้านลงไปเล้ย\n\
 pip!homework : โชว์listการบ้านที่เราได้เพิ่มไว้\n\
-pip!deadline : แสดงงานที่ต้องใกล้ส่งภายใน3วัน!!!```", inline=False )
+pip!deadline : แสดงงานที่ต้องใกล้ส่งภายใน3วัน!!!\n\
+pip!logout : ออกจากระบบ เพื่อหนีความจริง```", inline=False )
+        embed.set_thumbnail(url="https://www.apkmirror.com/wp-content/uploads/2021/07/09/60ed4ccf067f1.png") #URLภาพที่อยู่บนขวาของคำสั่ง help
 
         await ctx.channel.send(embed=embed)
 
---
+++ commands/logout.py
@@ -0,0 +1,25 @@
+import discord
+from discord import client
+from discord.ext import commands
+from settings import *
+import random
+
+list_logout = ["แล้วพบกันใหม่ เมื่อชาติต้องการ", "มีพบ ก็ต้องมีจาก แล้วเจอกันใหม่", "การบ้านใหม่ จะกลับมาในไม่ช้า"]
+
+class Logout(commands.Cog, name="Logout"):
+
+    def __init__(self, bot : commands.Bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def logout(self, ctx):
+        embed = discord.Embed(
+        title="ลาก่อนชาวโลก T______T", description=f"```{random.choice(list_logout)}```",
+             color=colorframe
+         )
+
+        await ctx.channel.send(embed=embed)
+        await self.bot.logout()
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Logout(bot))
