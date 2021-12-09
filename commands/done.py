de513abcaf63562d6f6699144c66eb7a358d8005
Tadporn <88502892+Tadporn@users.noreply.github.com>
2021-12-09 16:55:56 +0700

    Create done.py

---
+++ commands/done.py
@@ -0,0 +1,32 @@
+import discord
+import firebase_admin
+from discord.ext import commands
+from commands.homework import Homework
+from settings import *
+from firebase_admin import db
+
+class Done(commands.Cog, name="Done"):
+
+    def __init__(self, bot : commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    #*หลังctxคือการเอาข้อมูลหลังคำสั่งมาทั้งหมดแล้วใส่ไปในตัวแปลattr
+    async def done(self, ctx, *, attr = ""):
+        hw = attr
+        #ดึงข้อมูลมาจากDatabaseของFirebaseโดยทำการเรียกid discordของแต่ละคน
+        homeworks = db.reference(f"/{ctx.author.id}/homeworks/").get()
+        for key in homeworks:
+            if homeworks[key]['name'] == hw:
+                temp = key
+        db.reference(f"/{ctx.author.id}/homeworks/{temp}/").delete()
+
+        embed = discord.Embed(
+        title="เย่ทำเสร็จแล้วว\n",
+             color=colorframe
+        )
+
+        await ctx.channel.send(embed=embed)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Done(bot))
